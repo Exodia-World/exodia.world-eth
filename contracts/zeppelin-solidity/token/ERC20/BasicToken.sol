ebca26744207c7997ba23b438f3c3b3d37c137d4
Victor Huberta <victorhuberta@gmail.com>
2018-03-04 10:14:22 +0700

    Use eternal storage from EXOBase.

--- contracts/zeppelin-solidity/token/ERC20/BasicToken.sol
+++ contracts/zeppelin-solidity/token/ERC20/BasicToken.sol
@@ -2,47 +2,64 @@ pragma solidity 0.4.18;
 
 import "./ERC20Basic.sol";
 import "../../math/SafeMath.sol";
+import "../../../EXOBase.sol";
 
 /**
  * @title Basic token
  * @dev Basic version of StandardToken, with no allowances.
  */
-contract BasicToken is ERC20Basic {
+contract BasicToken is ERC20Basic, EXOBase {
   using SafeMath for uint256;
 
-  mapping(address => uint256) balances;
-
-  uint256 totalSupply_;
-
   /**
-  * @dev total number of tokens in existence
-  */
-  function totalSupply() public view returns (uint256) {
-    return totalSupply_;
-  }
-
-  /**
-  * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
-  */
+   * @dev transfer token for a specified address
+   * @param _to The address to transfer to.
+   * @param _value The amount to be transferred.
+   */
   function transfer(address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
-    require(_value <= balances[msg.sender]);
+    require(_value <= balanceOf(msg.sender));
 
     // SafeMath.sub will throw if there is not enough balance.
-    balances[msg.sender] = balances[msg.sender].sub(_value);
-    balances[_to] = balances[_to].add(_value);
+    balanceOf(msg.sender, balanceOf(msg.sender).sub(_value));
+    balanceOf(_to, balanceOf(_to).add(_value));
     Transfer(msg.sender, _to, _value);
     return true;
   }
 
   /**
-  * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of.
-  * @return An uint256 representing the amount owned by the passed address.
-  */
+   * @dev Get the balance of the specified address.
+   *
+   * @param _owner The address to query the the balance of
+   * @return An uint256 representing the amount owned by the passed address
+   */
   function balanceOf(address _owner) public view returns (uint256 balance) {
-    return balances[_owner];
+      return exoStorage.getUint(keccak256("token.balances", _owner, this));
+  }
+
+  /**
+   * @dev Get total number of tokens in existence.
+   */
+  function totalSupply() public view returns (uint256) {
+    return exoStorage.getUint(keccak256("token.totalSupply", this));
+  }
+
+  /**
+   * @dev Set the balance of the specified address.
+   *
+   * @param _owner The address to query the the balance of
+   * @param _balance The new balance owned by the passed address
+   */
+  function balanceOf(address _owner, uint256 _balance) internal {
+      exoStorage.setUint(keccak256("token.balances", _owner, this), _balance);
+  }
+
+  /**
+   * @dev Set total number of tokens in existence.
+   *
+   * @param _totalSupply //
+   */
+  function totalSupply(uint256 _totalSupply) internal {
+    exoStorage.setUint(keccak256("token.totalSupply", this), _totalSupply);
   }
 }
