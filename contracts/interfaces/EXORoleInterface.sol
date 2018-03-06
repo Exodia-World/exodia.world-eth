18938af63b3289c1bde4e4f71d5e248d61d68cd3
Victor Huberta <victorhuberta@gmail.com>
2018-03-06 21:09:23 +0700

    Add an interface for EXORole.

---
+++ contracts/interfaces/EXORoleInterface.sol
@@ -0,0 +1,17 @@
+pragma solidity 0.4.18;
+
+/**
+ * @title EXORole's Interface
+ *
+ * @dev An interface for the EXORole contract.
+ */
+interface EXORoleInterface {
+    modifier onlyLatestVersionOf(address _contract) {_;}
+    modifier onlyRole(string _role) {_;}
+    modifier onlySuperUser() {_;}
+
+    function transferOwnership(address _newOwner) public onlyLatestVersionOf(this) onlyRole("owner");
+    function roleTransfer(string _role, address _oldAddress, address _address) public onlyLatestVersionOf(this) onlySuperUser();
+    function roleAdd(string _role, address _address) public onlyLatestVersionOf(this) onlySuperUser();
+    function roleRemove(string _role, address _address) public onlyLatestVersionOf(this) onlySuperUser();
+}
