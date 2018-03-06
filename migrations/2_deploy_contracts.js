708a5503ae1cb8e42a8d1a6747b19d1b1926a579
Victor Huberta <victorhuberta@gmail.com>
2018-03-06 21:55:32 +0700

    Write first failing version of the deploy contract script.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,68 @@
-var EXOToken = artifacts.require("EXOToken");
+const EXOStorage = artifacts.require("EXOStorage");
+const EXOUpgrade = artifacts.require("EXOUpgrade");
+const EXORole = artifacts.require("EXORole");
+const EXOToken = artifacts.require("EXOToken");
 
-module.exports = function(deployer) {
-  deployer.deploy(
-    EXOToken,
-    100000000, // total supply
-    50000000, // minimum balance for stake reward
-    10000000, // locked treasury fund
-    5000000, // locked pre-sale fund
-    7300, // pre-sale price (1 ETH = 7300 EXO)
-    1209600, // pre-sale duration
-    25000000, // available ICO fund
-    3650, // minimum ICO tokens bought every purchase (1 ETH)
-    18250, // maximum ICO tokens bought for all purchases (5 ETH)
-    3650, // ICO price (1 ETH = 3650 EXO)
-    2419200, // ICO duration
-    10 // airdrop amount
-  );
+module.exports = async function(deployer) {
+  return deployer.deploy(EXOStorage).then(async () => {
+    await deployer.deploy(EXOUpgrade, EXOStorage.address);
+    await deployer.deploy(EXORole, EXOStorage.address);
+    await deployer.deploy(
+      EXOToken,
+      EXOStorage.address,
+      100000000, // total supply
+      50000000, // minimum balance for stake reward
+      10000000, // locked treasury fund
+      5000000, // locked pre-sale fund
+      1209600, // pre-sale duration
+      2419200, // ICO duration
+      25000000, // available ICO fund
+      3650, // minimum ICO tokens bought every purchase (1 ETH)
+      18250, // maximum ICO tokens bought for all purchases (5 ETH)
+      10 // airdrop amount
+    );
+
+    return EXOStorage.deployed().then(async exoStorage => {
+      // Register all contracts to EXOStorage in two different ways:
+      // 1. address => address -- allow us to validate it only with its address
+      // 2. name => address -- allow us to know its address by name
+
+      // Register EXOUpgrade contract.
+      await EXOStorage.setAddress(
+        web3.utils.sha3('contract.address', EXOUpgrade.address),
+        EXOUpgrade.address
+      );
+      await EXOStorage.setAddress(
+        web3.utils.sha3('contract.name', 'EXOUpgrade'),
+        EXOUpgrade.address
+      );
+
+      // Register EXORole contract.
+      await EXOStorage.setAddress(
+        web3.utils.sha3('contract.address', EXORole.address),
+        EXORole.address
+      );
+      await EXOStorage.setAddress(
+        web3.utils.sha3('contract.name', 'EXORole'),
+        EXORole.address
+      );
+
+      // Register EXOToken contract.
+      await EXOStorage.setAddress(
+        web3.utils.sha3('contract.address', EXOToken.address),
+        EXOToken.address
+      );
+      await EXOStorage.setAddress(
+        web3.utils.sha3('contract.name', 'EXOToken'),
+        EXOToken.address
+      );
+
+      // Disable direct access by owner to EXOStorage after initialization.
+      await EXOStorage.setBool(
+        web3.utils.sha3('contract.storage.initialized'),
+        true
+      );
+      return deployer;
+    });
+  });
 };
