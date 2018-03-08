cf3212cb9e5b13332383ee2784f32e1dbad01db3
Victor Huberta <victorhuberta@gmail.com>
2018-03-08 07:24:40 +0700

    Add newEXORole function and use soliditySha3.

--- test/TestEXORole.js
+++ test/TestEXORole.js
@@ -1,18 +1,40 @@
+const Web3Utils = require('web3-utils');
 const EXOStorage = artifacts.require('EXOStorage');
 const EXORole = artifacts.require('EXORole');
+
+const newEXORole = () => {
+  return EXOStorage.new().then(async exoStorage => {
+    const exoRole = await EXORole.new(EXOStorage.address);
+    // Register EXORole contract.
+    await exoStorage.setAddress(
+      Web3Utils.soliditySha3('contract.address', EXORole.address),
+      EXORole.address
+    );
+    await exoStorage.setAddress(
+      Web3Utils.soliditySha3('contract.name', 'EXORole'),
+      EXORole.address
+    );
+    // Disable direct access by owner to EXOStorage after initialization.
+    await exoStorage.setBool(
+      Web3Utils.soliditySha3('contract.storage.initialized'),
+      true
+    );
+    return exoRole;
+  });
+};
 
 contract('EXORole', accounts => {
   const owner = accounts[0];
 
   it('should transfer ownership to a new address if owner requests it', () => {
-    return EXORole.deployed().then(async exoRole => {
+    return newEXORole().then(async exoRole => {
       const exoStorage = await EXOStorage.deployed();
       const newOwner = accounts[3];
-      const isOwner = exoStorage.getBool.call(web3.sha3('access.role', 'owner', owner));
+      const isOwner = await exoStorage.getBool.call(Web3Utils.soliditySha3('access.role', 'owner', owner));
       assert(isOwner, 'Owner address should be correct');
 
       exoRole.transferOwnership(newOwner)
-        .then(result => {
+        .then(async result => {
           assert.equal(parseInt(result.receipt.status, 16), 1, 'The transfer should complete');
 
           for (let i = 0; i < result.logs.length; i++) {
@@ -23,8 +45,8 @@ contract('EXORole', accounts => {
             }
           }
 
-          const isOldAddressOwner = exoStorage.getBool.call(web3.sha3('access.role', 'owner', owner));
-          const isNewAddressOwner = exoStorage.getBool.call(web3.sha3('access.role', 'owner', newOwner));
+          const isOldAddressOwner = await exoStorage.getBool.call(Web3Utils.soliditySha3('access.role', 'owner', owner));
+          const isNewAddressOwner = await exoStorage.getBool.call(Web3Utils.soliditySha3('access.role', 'owner', newOwner));
           assert(! isOldAddressOwner, 'The old address should not be the owner anymore');
           assert(isNewAddressOwner, 'The new address should be the owner');
         });
